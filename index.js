7b29f9a8da5c6d21519944f9616208f94e19be9c
Abu Raihan <aburaihan8721@gmail.comm>
2021-10-30 00:11:19 +0600

    update server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World Server!");
+  res.send("Hello World Server trying to deploy!");
 });
 
 app.listen(port, () => {
